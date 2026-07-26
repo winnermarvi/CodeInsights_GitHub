93830271b947f0ae9bd686d8d096149da6efb994
winnermarvi <winnermarvi3@gmail.com>
2026-07-26 05:50:12 +0530

    Started chunking

---
+++ app/chunker/chunking.py
@@ -0,0 +1,57 @@
+from tree_sitter import Node
+
+
+def build_chunks(tree, relative_path):
+
+    chunks = []
+
+    def traverse(node):
+
+        # ---------- Function Chunk ----------
+        if node.type == "function_definition" and node.parent.type == "module":
+
+            function_name = ""
+
+            for child in node.children:
+
+                if child.type == "identifier":
+                    function_name = child.text.decode("utf-8")
+                    break
+
+            chunks.append({
+                "type": "function",
+                "name": function_name,
+                "content": node.text.decode("utf-8"),
+                "metadata": {
+                    "file": relative_path.name,
+                    "relative_path": str(relative_path)
+                }
+            })
+
+        # ---------- Class Chunk ----------
+        elif node.type == "class_definition":
+
+            class_name = ""
+
+            for child in node.children:
+
+                if child.type == "identifier":
+                    class_name = child.text.decode("utf-8")
+                    break
+
+            chunks.append({
+                "type": "class",
+                "name": class_name,
+                "content": node.text.decode("utf-8"),
+                "metadata": {
+                    "file": relative_path.name,
+                    "relative_path": str(relative_path)
+                }
+            })
+
+        for child in node.children:
+            traverse(child)
+
+    traverse(tree.root_node)
+
+    return chunks
